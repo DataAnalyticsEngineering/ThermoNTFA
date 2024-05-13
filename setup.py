7e145f9d8649b548930392e7f8c04c3d87ade8a2
juliusgh <juliusgh@users.noreply.github.com>
2024-05-13 15:30:12 +0000

    deploy: bf42cceb11392d61d4d571a0c4cbd80b6bc2a251

--- setup.py
+++
@@ -1,6 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="thermontfa",
-    packages=setuptools.find_packages(include=["thermontfa", "thermontfa.*"]),
-)
--- thermontfa/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-The python package `thermontfa` with our reference implementation of the thermo-plastic NTFA contains following classes:
-"""
-
-from .tabular_interpolation import *
-from .thermoNTFA import *
